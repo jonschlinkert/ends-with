4dfe8ef754d145e95b29642c458913522f24501a
tunnckoCore <mamemto_100@mail.bg>
2014-10-21 02:24:22 +0300

    some new tests

--- test.js
+++ test.js
@@ -39,4 +39,23 @@ describe('endsWith', function () {
     endsWith(['a', 'b', 42], '42').should.be.false;
     endsWith(['a', 'b', 42], 41).should.be.false;
   });
+
+  it('should be false if the array ends with the given object.', function () {
+    endsWith(['a', 'b', {'c': 'd'}], {'c': 'd'}).should.be.false;
+    endsWith(['a', 'b', {'c': 'd'}], {}).should.be.false;
+    endsWith(['a', 'b', {'c': 'd'}], 'd').should.be.false;
+  });
+
+  it('should be false if the array ends with the given array of strings.', function () {
+    endsWith(['a', 'b', ['c', 'd', 'e']], ['c', 'd', 'e']).should.be.false;
+    endsWith(['a', 'b', ['q', 'w', 'e']], ['c', 'd', 'e']).should.be.false;
+    endsWith(['a', 'b', 'c'], ['c']).should.be.false;
+  });
+
+  it('should be false if the array ends with the given array of object(s).', function () {
+    endsWith(['a', 'b', [{'c': 'd'}]], [{'c': 'd'}]).should.be.false;
+    endsWith(['a', 'b', [{'q': 'w'}, {'e': 'f'}]], [{'q': 'w'}, {'e': 'f'}]).should.be.false;
+    endsWith(['a', 'b', [{'q': 'w'}, {'e': 'f'}]], [{'e': 'f'}, {'q': 'w'}]).should.be.false;
+    endsWith(['a', 'b', [{'q': 'w'}, {'e': 'f'}]], [{'e': 'f'}]).should.be.false;
+  });
 });
